2159b88b12424cc30828469f850154c4839f69d4
Don Foh Cher Ern <don_foh@hotmail.com>
2024-05-25 21:03:56 +0800

    added experience descriptions

--- src/components/ExperienceCard.jsx
+++ src/components/ExperienceCard.jsx
@@ -7,7 +7,23 @@ import {
     CardTitle,
 } from "@/components/ui/card"
 
-const ExperienceCard = ({ company, title, duration, description, tech }) => {
+import {
+    Collapsible,
+    CollapsibleContent,
+    CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+
+import { Button } from "./ui/button"
+import { CaretSortIcon } from "@radix-ui/react-icons"
+
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion"
+
+const ExperienceCard = ({ company, title, duration, description, tech, techDetails }) => {
     return (
         <Card className="w-full max-w-[800px]">
             <CardHeader>
@@ -20,6 +36,32 @@ const ExperienceCard = ({ company, title, duration, description, tech }) => {
             </CardHeader>
             <CardContent>
                 {description}
+                <Accordion type="single" collapsible className="w-full">
+                    {
+                        techDetails.map((td, idx) => 
+                            <AccordionItem value={td.detailTitle} key={idx}>
+                                <AccordionTrigger>{td.detailTitle}</AccordionTrigger>
+                                <AccordionContent className="flex flex-col gap-2">
+                                    {td.detailDescription.map((dd, idx) => <p key={idx}>{dd}</p>)}
+                                </AccordionContent>
+                            </AccordionItem>
+                        )
+                    }
+
+                </Accordion>
+                {/* <Collapsible className="mt-8">
+                    <CollapsibleTrigger>
+                        <Button variant="ghost" size="sm">
+                            <CaretSortIcon className="h-4 w-4" />
+                            <strong>
+                                Technical Details (Click to toggle)
+                            </strong>
+                        </Button>
+                    </CollapsibleTrigger>
+                    <CollapsibleContent>
+                        {techDetails}
+                    </CollapsibleContent>
+                </Collapsible> */}
             </CardContent>
             <CardFooter>
                 <span className="font-semibold">Technologies:</span>
